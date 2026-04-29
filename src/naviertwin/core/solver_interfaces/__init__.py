cee48fc672e2d01e21a5447d682be8a0cc5fc8be
younglin90 <younglin90@gmail.com>
2026-04-30 02:23:42 +0900

    docs: expose solver benchmark public APIs

--- src/naviertwin/core/solver_interfaces/__init__.py
+++ src/naviertwin/core/solver_interfaces/__init__.py
@@ -5,3 +5,33 @@
 - ``flowtorch_wrapper`` : flowtorch 기반 POD/DMD 파이프라인 연동
 - ``jax_fluids_wrapper`` : JAX-Fluids 선호
 """
+
+from naviertwin.core.solver_interfaces.fvm_advection import (
+    fvm_musclhancock_1d,
+    fvm_upwind_1d,
+    minmod,
+    total_mass,
+)
+from naviertwin.core.solver_interfaces.lbm_d2q9 import LBMD2Q9
+from naviertwin.core.solver_interfaces.pde_solvers import (
+    solve_burgers_1d,
+    solve_heat_1d,
+)
+from naviertwin.core.solver_interfaces.sph import (
+    cubic_spline_kernel,
+    sph_density_1d,
+    sph_gradient_1d,
+)
+
+__all__ = [
+    "LBMD2Q9",
+    "cubic_spline_kernel",
+    "fvm_musclhancock_1d",
+    "fvm_upwind_1d",
+    "minmod",
+    "solve_burgers_1d",
+    "solve_heat_1d",
+    "sph_density_1d",
+    "sph_gradient_1d",
+    "total_mass",
+]
